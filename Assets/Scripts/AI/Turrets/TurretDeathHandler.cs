1126996740ad9de036da57294093a95311eed6d3
ArminPosadas <armin.posadas@uniat.edu.mx>
2025-09-26 14:01:59 -0600

    Re-añadiendo lo que se elimino

--- Assets/Scripts/AI/Turrets/TurretDeathHandler.cs
+++ Assets/Scripts/AI/Turrets/TurretDeathHandler.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class TurretDeathHandler : MonoBehaviour
 {
     private Health _health;
+    [SerializeField] public GameObject barPrefab;
+    private GameObject barInstance;
 
     void Start()
     {
@@ -14,6 +16,13 @@ public class TurretDeathHandler : MonoBehaviour
 
     void HandleDeath()
     {
+        SpawnUpgrade();
         Destroy(gameObject);
+    }
+    
+    void SpawnUpgrade()
+    {
+        Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y + 0.2f , transform.position.z);
+        barInstance = Instantiate(barPrefab, spawnPosition, transform.rotation);
     }
 }
